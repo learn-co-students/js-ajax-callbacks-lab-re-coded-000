acf023fc06dfb4c0c42d150e4a866b8453eaddd1
Ali Alrawi <ali.developer90@gmail.com>
2017-01-12 20:26:36 +0300

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
 function replaceNouns() {
+	request("/noun.html",".noun")
 };
 
 function replaceVerbs() {
+	request("/verb.html",".verb")
 };
+function request(url,class_){
+  var words=[];
+    $.get(url, function(data) {
+     $.each(data.split("\n"), function(i, item) {
+     
+       words.push(item);
+    });  
+
+//////////
+     $(class_).each(function(index,span){
+                 $(this).html(words[Math.floor(Math.random() * (words.length-1))])
+                                         })
+
+ //////////                                        
+     }).fail(function(error) {alert('Something went wrong: ' + error);})
+
+    //
+
+
+
+}
 
 $(document).ready(function (){
-  // Code here
+    $('#random_noun').on('click',function(){
+    replaceNouns()
+  })
+  ///////
+  $('#random_verb').on('click',function(){
+    replaceVerbs()
+  })
 });
